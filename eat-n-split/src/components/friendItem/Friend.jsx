64d64a27fc262f93e24eae03ffbce8782226d7b2
peaceGloriole <dmtrpwlw@icloud.com>
2024-09-07 14:20:21 +0300

    list of friends

---
+++ eat-n-split/src/components/friendItem/Friend.jsx
@@ -0,0 +1,21 @@
+export default function Friend({ friend }) {
+  const balanceColor = (balance) => {
+    if (balance === 0) return "";
+    return balance < 0 ? `red` : `green`;
+  };
+
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      <p className={balanceColor(friend.balance)}>
+        {friend.balance === 0
+          ? `You and ${friend.name} are even`
+          : friend.balance < 0
+          ? `You owe ${friend.name} ${Math.abs(friend.balance)}$`
+          : `${friend.name} owes you ${Math.abs(friend.balance)}$`}
+      </p>
+      <button className="button">Select</button>
+    </li>
+  );
+}
