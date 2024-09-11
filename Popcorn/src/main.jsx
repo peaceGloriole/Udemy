4277133d7e9f7a61d8318b940d74c2405c131bef
peaceGloriole <dmtrpwlw@icloud.com>
2024-09-11 08:47:26 +0300

    Star component

--- Popcorn/src/main.jsx
+++ Popcorn/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import Star from "./Star.jsx";
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Star maxRating={5}/>
+    <Star maxRating={10}/>
   </React.StrictMode>
 );
