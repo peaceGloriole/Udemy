622eac4a4a082c0fcb3ae8e98884e1ec10e078a5
peaceGloriole <dmtrpwlw@icloud.com>
2024-09-19 15:02:44 +0300

    clean up project

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+      <select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
+
+export default App;
