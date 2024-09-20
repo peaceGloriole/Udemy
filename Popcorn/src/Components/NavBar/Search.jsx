46414f366a9a2c98ed412e6f7fa2084bf7785f21
peaceGloriole <dmtrpwlw@icloud.com>
2024-09-20 18:12:39 +0300

    lil feauture

--- Popcorn/src/Components/NavBar/Search.jsx
+++ Popcorn/src/Components/NavBar/Search.jsx
@@ -4,7 +4,17 @@ export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(() => {
-    inputEl.current.focus();
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.key === `Enter`) {
+        inputEl.current.focus();
+        setQuery(``);
+      }
+    };
+
+    document.addEventListener(`keydown`, callback);
+    return () => document.removeEventListener(`keydown`, callback);
   }, []);
 
   return (
