a2b1dbe4d60ec9b93729345d19a6580361c3ec79
peaceGloriole <dmtrpwlw@icloud.com>
2024-09-23 12:25:32 +0300

    server fixed

--- AdvancedReact/react-quiz/src/App.jsx
+++ AdvancedReact/react-quiz/src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./Header";
+import MainComp from "./MainComp";
+
+import questions from "./data/questions.json" assert { type: "json" };
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <MainComp>
+        <p>1/15</p>
+      </MainComp>
     </div>
   );
 }
