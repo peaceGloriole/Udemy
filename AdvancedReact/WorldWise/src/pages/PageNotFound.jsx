44f6c5cac90612fcc5a44e5de225f2a82c2cb174
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-12 18:58:57 +0300

    resource added

--- AdvancedReact/WorldWise/src/pages/PageNotFound.jsx
+++ AdvancedReact/WorldWise/src/pages/PageNotFound.jsx
@@ -1,3 +1,7 @@
-export default function NotFound() {
-  return <div>Not Found</div>;
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
