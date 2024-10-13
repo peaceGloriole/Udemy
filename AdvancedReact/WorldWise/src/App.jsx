88b602771087ed0958c70123a3093d8a2982663f
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-13 21:10:20 +0300

    Refactor CityList component to include a "Go Back" button

--- AdvancedReact/WorldWise/src/App.jsx
+++ AdvancedReact/WorldWise/src/App.jsx
@@ -42,7 +42,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
