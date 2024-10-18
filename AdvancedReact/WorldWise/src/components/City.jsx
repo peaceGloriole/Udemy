fec4ee99f6614f3f58d28efc6c9468de108ca197
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-18 19:15:36 +0300

    Refactor City component to use useNavigate hook for navigation and add a "Go Back" button

--- AdvancedReact/WorldWise/src/components/City.jsx
+++ AdvancedReact/WorldWise/src/components/City.jsx
@@ -1,8 +1,11 @@
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import styles from "./City.module.css";
 import { useCities } from "../context/CitiesContext";
 import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import styles from "./City.module.css";
+
 import Button from "./Button";
+import Spinner from "./Spinner";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -15,13 +18,17 @@ const formatDate = (date) =>
 function City() {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { fetchCity, curCity } = useCities();
+  const { fetchCity, curCity, isLoading } = useCities();
 
   useEffect(() => {
     fetchCity(id);
   }, [id]);
 
   const { cityName, emoji, date, notes } = curCity;
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div className={styles.city}>
