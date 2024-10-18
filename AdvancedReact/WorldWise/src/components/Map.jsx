4d18cd708f7a8e9eddd6ca75f668e1917954725e
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-18 21:04:18 +0300

    Refactor package dependencies and update Leaflet version

--- AdvancedReact/WorldWise/src/components/Map.jsx
+++ AdvancedReact/WorldWise/src/components/Map.jsx
@@ -1,18 +1,37 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import { useCities } from "../context/CitiesContext";
+
 export default function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate(`form`)}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        className={styles.map}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.id}
+            position={[city.position.lat, city.position.lng]}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
