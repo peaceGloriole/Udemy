36b18428c202acf9bf1f318f94ac0e4c0d35a46a
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-18 17:01:20 +0300

    Refactor App.jsx to use CitiesProvider for managing cities data

---
+++ AdvancedReact/WorldWise/src/context/CitiesContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useEffect, useState } from "react";
+
+const URL = `http://localhost:9000`;
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Failed to fetch data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
