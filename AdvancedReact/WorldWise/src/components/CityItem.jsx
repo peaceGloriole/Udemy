4a756f3d35616d626765f0afabcead990c5ded89
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-19 14:12:14 +0300

    Refactor CityItem component to handle delete functionality

--- AdvancedReact/WorldWise/src/components/CityItem.jsx
+++ AdvancedReact/WorldWise/src/components/CityItem.jsx
@@ -13,6 +13,12 @@ export default function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
   const { curCity, deleteCity } = useCities();
 
+  function handleDelete(e) {
+    e.preventDefault();
+
+    deleteCity(id);
+  }
+
   return (
     <li>
       <Link
@@ -24,7 +30,7 @@ export default function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn} onClick={() => deleteCity(id)}>
+        <button className={styles.deleteBtn} onClick={handleDelete}>
           &times;
         </button>
       </Link>
