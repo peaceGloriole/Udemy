ebf48dc258fe695cf8fe245c69ba872164a3e7ab
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-27 16:42:34 +0200

    acc/cust reducers/redux

--- AdvancedReact/ReduxIntro/redux-intro/src/store.js
+++ AdvancedReact/ReduxIntro/redux-intro/src/store.js
@@ -1,10 +1,25 @@
-const initialState = {
+import { combineReducers, createStore } from "redux";
+
+const initialStateAccount = {
   balance: 0,
   loan: 0,
-  loanPurpose: "",
+  loanPurpose: ``,
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: ``,
+  ID: ``,
+  createdAt: ``,
+};
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case `account/deposit`:
       return {
@@ -22,7 +37,9 @@ function reducer(state = initialState, action) {
       }
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case `account/payLoan`:
       return {
@@ -31,7 +48,57 @@ function reducer(state = initialState, action) {
         loanPurpose: ``,
         balance: state.balance - state.loan,
       };
+
     default:
       return state;
   }
+}
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case `customer/createCustomer`:
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        ID: action.payload.ID,
+        createdAt: action.payload.createdAt,
+      };
+    case `customer/updateCustomer`:
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function deposit(amount) {
+  return { type: `account/deposit`, payload: amount };
+}
+function withdraw(amount) {
+  return { type: `account/withdraw`, payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: `account/requestLoan`,
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: `account/payLoan` };
+}
+
+function createCustomer(fullName, ID) {
+  return {
+    type: `customer/createCustomer`,
+    payload: { fullName, ID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateCustomer(fullName) {
+  return {
+    type: `customer/updateCustomer`,
+    payload: { fullName },
+  };
 }
