29941e8a570470b4be83f9caf7854d18a45f2bf3
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-27 18:07:23 +0200

    Refactor CreateCustomer and Customer components to use redux for state management

--- AdvancedReact/ReduxIntro/redux-intro/src/features/customers/Customer.js
+++ AdvancedReact/ReduxIntro/redux-intro/src/features/customers/Customer.js
@@ -3,7 +3,11 @@ import { useSelector } from "react-redux";
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
 
-  return customer ? <h2>👋 Welcome, {customer} </h2> : <h2>👋 Welcome </h2>;
+  return customer ? (
+    <h2>👋 Welcome, {customer} </h2>
+  ) : (
+    <h2>👋 Welcome, guest </h2>
+  );
 }
 
 export default Customer;
