192fc5d0ae93b78bd09c0f5c91414eb8db9de402
peaceGloriole <dmtrpwlw@icloud.com>
2024-10-27 16:49:14 +0200

    Refactor account and customer reducers in store.js and move them to separate files

---
+++ AdvancedReact/ReduxIntro/redux-intro/src/features/customers/customerSlice.js
@@ -0,0 +1,40 @@
+const initialStateCustomer = {
+  fullName: ``,
+  ID: ``,
+  createdAt: ``,
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case `customer/createCustomer`:
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        ID: action.payload.ID,
+        createdAt: action.payload.createdAt,
+      };
+    case `customer/updateCustomer`:
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, ID) {
+  return {
+    type: `customer/createCustomer`,
+    payload: { fullName, ID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateCustomer(fullName) {
+  return {
+    type: `customer/updateCustomer`,
+    payload: { fullName },
+  };
+}
+
+export { customerReducer, createCustomer, updateCustomer };
