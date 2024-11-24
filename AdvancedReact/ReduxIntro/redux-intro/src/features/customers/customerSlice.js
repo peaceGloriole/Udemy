2aa5cff641c41ce4b527f5d44196100f4464f8aa
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-24 15:18:26 +0200

    Refactor customerSlice to use createSlice from Redux Toolkit; streamline customer actions and state management

--- AdvancedReact/ReduxIntro/redux-intro/src/features/customers/customerSlice.js
+++ AdvancedReact/ReduxIntro/redux-intro/src/features/customers/customerSlice.js
@@ -1,40 +1,73 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: ``,
   ID: ``,
   createdAt: ``,
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case `customer/createCustomer`:
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        ID: action.payload.ID,
-        createdAt: action.payload.createdAt,
-      };
-    case `customer/updateCustomer`:
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+const customerSlice = createSlice({
+  name: `customer`,
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, ID) {
+        return {
+          payload: {
+            fullName,
+            ID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
 
-function createCustomer(fullName, ID) {
-  return {
-    type: `customer/createCustomer`,
-    payload: { fullName, ID, createdAt: new Date().toISOString() },
-  };
-}
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.ID = action.payload.ID;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateCustomer(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-function updateCustomer(fullName) {
-  return {
-    type: `customer/updateCustomer`,
-    payload: fullName,
-  };
-}
+export const { createCustomer, updateCustomer } = customerSlice.actions;
 
-export { createCustomer, updateCustomer };
+export default customerSlice.reducer;
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case `customer/createCustomer`:
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         ID: action.payload.ID,
+//         createdAt: action.payload.createdAt,
+//       };
+//     case `customer/updateCustomer`:
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// function createCustomer(fullName, ID) {
+//   return {
+//     type: `customer/createCustomer`,
+//     payload: { fullName, ID, createdAt: new Date().toISOString() },
+//   };
+// }
+
+// function updateCustomer(fullName) {
+//   return {
+//     type: `customer/updateCustomer`,
+//     payload: fullName,
+//   };
+// }
+
+// export { createCustomer, updateCustomer };
