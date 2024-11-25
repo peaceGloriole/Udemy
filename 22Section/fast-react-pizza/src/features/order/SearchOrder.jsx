e69e301137a1c96958539ae3f2467c5ade146a10
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-25 19:02:54 +0200

    Add search functionality to SearchOrder component; implement navigation on form submission

--- 22Section/fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 22Section/fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchOrder() {
   const [query, setQuery] = useState(``);
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!query) {
+      return;
+    }
+
+    navigate(`/order/${query}`);
   }
 
   return (
@@ -14,6 +21,7 @@ export default function SearchOrder() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      <button>Search</button>
     </form>
   );
 }
