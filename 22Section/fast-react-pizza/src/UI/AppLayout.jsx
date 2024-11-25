7bda216caf9cbaa6760c92fb6bdae821916ebf12
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-25 18:40:16 +0200

    Add loading state to AppLayout; implement Loader component and remove console log from Menu

--- 22Section/fast-react-pizza/src/UI/AppLayout.jsx
+++ 22Section/fast-react-pizza/src/UI/AppLayout.jsx
@@ -1,18 +1,27 @@
-import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+
+import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
-      <Header />
-
-      <main>
-        <Outlet />
-      </main>
-
-      <CartOverview />
+    <div className="layout">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+          <CartOverview />
+        </>
+      )}
     </div>
   );
 }
