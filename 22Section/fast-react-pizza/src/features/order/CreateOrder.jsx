6d57240abaf95a3e60a748d02a920f462e140464
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-26 17:55:48 +0200

    Enhance CreateOrder component with order submission handling; integrate loading state and API call for order creation

--- 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -32,6 +33,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === `submitting`;
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -71,7 +74,10 @@ function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? `Placing your order..` : `Order now`}
+          </button>
         </div>
       </Form>
     </div>
@@ -82,8 +88,14 @@ export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === `on`,
+  };
 
-  return null;
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 export default CreateOrder;
