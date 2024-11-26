5272386041427b1c2a03b88300aa4e994ae3c743
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-26 18:25:56 +0200

    Add form validation to CreateOrder component; implement phone number error handling

--- 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect, useNavigation } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -38,6 +38,8 @@ function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
+  const formErrors = useActionData();
+
   return (
     <div>
       <h2>Ready to order? Let`s go!</h2>
@@ -53,6 +55,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -93,6 +96,15 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart),
     priority: data.priority === `on`,
   };
+
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = `Invalid phone number, please give us your correct phone number, we might need it to contact you.`;
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  }
 
   const newOrder = await createOrder(order);
 
