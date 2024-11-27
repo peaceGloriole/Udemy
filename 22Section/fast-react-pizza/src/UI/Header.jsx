377f4aaafe8dbafb483487478b68d816373d0936
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-27 08:08:07 +0200

    Add Prettier and Tailwind CSS plugin; update styles in Header, Home, and CartOverview components

--- 22Section/fast-react-pizza/src/UI/Header.jsx
+++ 22Section/fast-react-pizza/src/UI/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast Pizza Co.</Link>
 
       <SearchOrder />
