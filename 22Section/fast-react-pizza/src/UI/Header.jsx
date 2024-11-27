60660a81bd178c8ccea602ccbd9596d68a1c530d
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-27 09:33:38 +0200

    Enhance responsive design; update Header, Home, CartOverview, and CreateUser components with improved spacing and styling

--- 22Section/fast-react-pizza/src/UI/Header.jsx
+++ 22Section/fast-react-pizza/src/UI/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="border-b-2 border-stone-700 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b-2 border-stone-700 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="text-center font-extrabold tracking-[8px]">
         Fast Pizza Co.
       </Link>
