f07c15953662ab88c91656cbbb2d68e360ba642c
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-27 09:20:03 +0200

    Enhance UI components with improved styling; add CreateUser component to Home, update Header and CartOverview styles, and hide Username display

--- 22Section/fast-react-pizza/src/UI/Header.jsx
+++ 22Section/fast-react-pizza/src/UI/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="border-b-2 border-stone-700 bg-yellow-500 px-4 py-3 uppercase">
       <Link to="/" className="text-center font-extrabold tracking-[8px]">
         Fast Pizza Co.
       </Link>
