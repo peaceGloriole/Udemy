78622817dbb6439dc629eda9488dd344650a227d
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-30 11:37:47 +0200

    Integrate Redux for user state management; update Username component to display dynamic username and wrap App in Provider

--- 22Section/fast-react-pizza/src/features/user/Username.jsx
+++ 22Section/fast-react-pizza/src/features/user/Username.jsx
@@ -1,3 +1,13 @@
+import { useSelector } from "react-redux";
+
 export default function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Radi</div>;
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) {
+    return;
+  }
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
