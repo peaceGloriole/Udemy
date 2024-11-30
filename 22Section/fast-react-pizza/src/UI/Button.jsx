13790e51f3a0ae482147f7ebd24b507440508767
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-30 10:20:32 +0200

    Enhance UI components with improved styling and layout; update button styles and form structure for better user experience

--- 22Section/fast-react-pizza/src/UI/Button.jsx
+++ 22Section/fast-react-pizza/src/UI/Button.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
-const base = `duration-400 mt-1 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-600 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-600 active:text-yellow-400`;
+const base = `duration-400 text-sm mt-1 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-600 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-600 active:text-yellow-400`;
 export default function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ` px-4 py-2 md:px-4 md:py-2`,
     small: base + ` py-2 px-3 md:px-3 md:py-2 text-xs`,
-    secondary: ` py-2 px-3 md:px-3 md:py-2 duration-400 mt-1 inline-block rounded-full bg-stone-300 font-semibold uppercase tracking-wide text-stone-600 transition-colors hover:bg-stone-400 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 active:bg-stone-400 active:text-stone-100`,
+    secondary: ` text-sm py-2 px-3 md:px-3 md:py-2 duration-400 mt-1 inline-block rounded-full bg-stone-300 font-semibold uppercase tracking-wide text-stone-600 transition-colors hover:bg-stone-400 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 active:bg-stone-400 active:text-stone-100`,
   };
 
   if (to) {
--- 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -45,40 +45,54 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   return (
-    <div>
-      <h2>Ready to order? Let`s go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-6 text-xl font-semibold">Ready to order? Let`s go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="mb-3 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="mb-3 flex flex-col gap-2 sm:flex-row sm:items-center">
+          {formErrors?.phone ? (
+            <label className="text-red-500 sm:basis-40">Phone number</label>
+          ) : (
+            <label className="sm:basis-40">Phone number</label>
+          )}
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+
+            {formErrors?.phone && (
+              <p className="mt-1 rounded-xl bg-red-200 px-4 py-1 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-3 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex items-center gap-1">
           <input
-            className="mr-1 mt-1 h-4 w-4 accent-yellow-400"
+            className="mr-1 mt-1 h-4 w-5 accent-yellow-400"
             type="checkbox"
             name="priority"
             id="priority"
             checked={withPriority}
             onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">
+          <label className="mt-1 font-medium" htmlFor="priority">
             {withPriority
               ? `Your order will be prioritized`
               : `Want to give your order priority?`}
@@ -115,8 +129,9 @@ export async function action({ request }) {
     return errors;
   }
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 export default CreateOrder;
