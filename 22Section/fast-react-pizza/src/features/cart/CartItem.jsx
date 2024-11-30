29484f78872f814f96f6fa7bd0d2d65dac86d702
peaceGloriole <dmtrpwlw@icloud.com>
2024-11-30 17:20:32 +0200

    Refactor cart item deletion; replace decrement quantity functionality with delete item action in CartItem component

--- 22Section/fast-react-pizza/src/features/cart/CartItem.jsx
+++ 22Section/fast-react-pizza/src/features/cart/CartItem.jsx
@@ -2,11 +2,17 @@
 import { useDispatch } from "react-redux";
 import Button from "../../UI/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { decItemQty } from "./cartSlice";
+import { deleteItem } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
   const dispatch = useDispatch();
+
+  const deleteHandler = (id) => {
+    return () => {
+      dispatch(deleteItem(id));
+    };
+  };
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -15,7 +21,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-semibold">{formatCurrency(totalPrice)}</p>
-        <Button type="small" onClick={() => dispatch(decItemQty())}>
+        <Button type="small" onClick={deleteHandler(pizzaId)}>
           Изтрий
         </Button>
       </div>
