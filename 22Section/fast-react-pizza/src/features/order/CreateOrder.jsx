07277bea246a73966b2004c7644475dce9e93f59
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-05 18:53:10 +0200

    geo not finished

--- 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 22Section/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,10 +3,11 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { useState } from "react";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../UI/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -18,6 +19,7 @@ function CreateOrder() {
   const username = useSelector((state) => state.user.username);
   const [withPriority, setWithPriority] = useState(false);
 
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === `submitting`;
 
@@ -32,6 +34,8 @@ function CreateOrder() {
       <h2 className="mb-6 text-xl font-semibold">
         Готов ли си да поръчаш? Да започваме!
       </h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
 
       <Form method="POST">
         <div className="mb-3 flex flex-col gap-2 sm:flex-row sm:items-center">
