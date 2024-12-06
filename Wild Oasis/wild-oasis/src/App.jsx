3126da4b96063cb284fcac1ed2d4dd389ff984f3
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-06 20:08:14 +0200

    Add styled-components for styling; implement GlobalStyles and refactor App component layout with new UI components

--- Wild Oasis/wild-oasis/src/App.jsx
+++ Wild Oasis/wild-oasis/src/App.jsx
@@ -1,10 +1,49 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  color: red;
-  font-weight: bold;
+const StyledApp = styled.main`
+  padding: 20px;
 `;
 
 export default function App() {
-  return <H1>Hello World</H1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("Checked in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Checked out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="text" placeholder="Enter your name" />
+              <Input type="text" placeholder="Enter your name" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
