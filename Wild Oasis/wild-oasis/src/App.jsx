db1fe15005112d1cde13dce2b73234a45ac590ca
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-06 08:36:29 +0200

    Remove ESLint configuration file and add styled-components dependency; update App component to use styled-components for styling

--- Wild Oasis/wild-oasis/src/App.jsx
+++ Wild Oasis/wild-oasis/src/App.jsx
@@ -1,3 +1,10 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  color: red;
+  font-weight: bold;
+`;
+
 export default function App() {
-  return <div>Hello World</div>;
+  return <H1>Hello World</H1>;
 }
--- Wild Oasis/wild-oasis/vite.config.js
+++ Wild Oasis/wild-oasis/vite.config.js
@@ -4,5 +4,11 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      // Specify the ESLint configuration file
+      overrideConfigFile: ".eslintrc.json",
+    }),
+  ],
 });
