f9d85b1cc8822901ead98e23e14f98a6fc0cc4bc
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-06 21:07:41 +0200

    building applayout, sidebar and header

--- Wild Oasis/wild-oasis/src/App.jsx
+++ Wild Oasis/wild-oasis/src/App.jsx
@@ -10,6 +10,7 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFoung from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 export default function App() {
   return (
@@ -17,14 +18,15 @@ export default function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFoung />} />
         </Routes>
