51758c4f43d2e6ac225984f4fd5f0df0f6cc9c46
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-07 20:51:38 +0200

    error handling in forms

--- Wild Oasis/wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ Wild Oasis/wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -48,7 +48,9 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+
   const queryClient = useQueryClient();
   const { isLoading, mutate } = useMutation({
     mutationFn: (newCabin) => createCabin(newCabin),
@@ -68,21 +70,54 @@ function CreateCabinForm() {
     mutate(data);
   }
 
+  function onError(errors) {
+    // console.log(errors);
+  }
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register(`name`)} />
+        <Input
+          type="text"
+          id="name"
+          {...register(`name`, {
+            required: `This field is required`,
+          })}
+        />
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register(`maxCapacity`)} />
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register(`maxCapacity`, {
+            required: `This field is required`,
+            min: {
+              value: 1,
+              message: `Capacity must be at least 1`,
+            },
+          })}
+        />
+        {errors?.maxCapacity?.message && (
+          <Error>{errors.maxCapacity.message}</Error>
+        )}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register(`regularPrice`)} />
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register(`regularPrice`, {
+            required: `This field is required`,
+          })}
+        />
+        {errors?.regularPrice?.message && (
+          <Error>{errors.regularPrice.message}</Error>
+        )}
       </FormRow>
 
       <FormRow>
@@ -91,8 +126,19 @@ function CreateCabinForm() {
           type="number"
           id="discount"
           defaultValue={0}
-          {...register(`discount`)}
+          {...register(`discount`, {
+            required: `This field is required`,
+            validate: (value) => {
+              const discountValue = parseFloat(value);
+              const regularPriceValue = parseFloat(getValues().regularPrice);
+              return (
+                discountValue <= regularPriceValue ||
+                `Discount should be less than regular price!`
+              );
+            },
+          })}
         />
+        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </FormRow>
 
       <FormRow>
@@ -101,8 +147,13 @@ function CreateCabinForm() {
           type="number"
           id="description"
           defaultValue=""
-          {...register(`description`)}
+          {...register(`description`, {
+            required: `This field is required`,
+          })}
         />
+        {errors?.description?.message && (
+          <Error>{errors.description.message}</Error>
+        )}
       </FormRow>
 
       <FormRow>
