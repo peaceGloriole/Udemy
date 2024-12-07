024cb32e65e41fae3c3831e20e0abbd89652a738
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-07 15:46:00 +0200

    Add cabin management features with React Query integration

--- Wild Oasis/wild-oasis/src/App.jsx
+++ Wild Oasis/wild-oasis/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import GlobalStyles from "./styles/GlobalStyle";
 
 import Dashboard from "./pages/Dashboard";
@@ -10,13 +13,22 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFoung from "./pages/PageNotFound";
+
 import AppLayout from "./ui/AppLayout";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 10 * 1000,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
@@ -32,6 +44,6 @@ export default function App() {
           <Route path="*" element={<PageNotFoung />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
