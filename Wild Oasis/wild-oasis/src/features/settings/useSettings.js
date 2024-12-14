845ab765618a22cb9ec78340f1d9f7616dfdc39c
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-14 17:39:26 +0200

    some bug fixes

--- Wild Oasis/wild-oasis/src/features/settings/useSettings.js
+++ Wild Oasis/wild-oasis/src/features/settings/useSettings.js
@@ -1,5 +1,5 @@
 import { getSettings } from "../../services/apiSettings";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 export function useSettings() {
   const {
