8643ad48a531192e2f204368c7cf761578248d72
peaceGloriole <dmtrpwlw@icloud.com>
2024-12-14 17:48:19 +0200

    fetching settings

--- Wild Oasis/wild-oasis/src/features/settings/UpdateSettingsForm.jsx
+++ Wild Oasis/wild-oasis/src/features/settings/UpdateSettingsForm.jsx
@@ -3,23 +3,45 @@ import { useSettings } from "./useSettings";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
 
 function UpdateSettingsForm() {
-  const { isLoading, settings } = useSettings();
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+    //empty object but working without it. Just in case
+  } = useSettings();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" />
+        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" />
+        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" />
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" />
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
